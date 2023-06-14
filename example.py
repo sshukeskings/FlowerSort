9f13e69e7473a58486647bb32d05b2bf826496f3
Scott Shukes <91894767+sshukeskings@users.noreply.github.com>
2023-06-14 07:55:26 -0400

    Create example.py

---
+++ example.py
@@ -0,0 +1,8 @@
+from flower_sort import flower_sort
+
+# Example usage of flower_sort function
+numbers = [4, 2, 7, 1, 3]
+sorted_numbers = flower_sort(numbers)
+
+print("Original list:", numbers)
+print("Sorted list:", sorted_numbers)
